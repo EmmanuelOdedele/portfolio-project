cf3e0a3d7571054ce08fc64cdb23b980daa545ea
EmmanuelOdedele <emmaodedele@gmail.com>
2023-03-15 03:02:38 +0000

    initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/works">Works</router-link> |
+    <router-link to="/resume">Resume</router-link> |
+    <router-link to="/contact">Contact</router-link> 
   </nav>
   <router-view/>
 </template>
---
+++ src/views/Resume.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Resume</h1>
+</template>
+
+<script>
+export default {
+  name: "Resume",
+  components: {},
+};
+</script>
